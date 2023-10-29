9bf0c8db755d8b900faf1e4a94c68f6fc17223a4
Nau-c <nlopez@desic-sl.com>
2023-10-29 14:37:50 +0000

    script is added to show the number and element control is added to be shown

---
+++ Master Front End XV - Modulo 1 - Layout - Reto/JS/script.js
@@ -0,0 +1,15 @@
+// Función para cambiar el contenido según el tamaño de la pantalla
+function changeTextSecondScreenSize() {
+    const elemento = document.getElementById('top-text');
+    
+    if (window.innerWidth < 1280) {
+      elemento.textContent = "3";
+    } else {
+      elemento.textContent = "5";
+    }
+  }
+  
+  // Ejecuta la función cuando la página se carga y cuando la ventana se redimensiona
+  window.addEventListener('load', changeTextSecondScreenSize);
+  window.addEventListener('resize', changeTextSecondScreenSize);
+  
